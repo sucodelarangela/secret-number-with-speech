7ed4f981ff3963dddb3233dfb5befc1628f2855d
Angela Caldas <aes.caldas@gmail.com>
2022-09-27 17:59:25 -0300

    draw secret number

---
+++ app/drawNumber.js
@@ -0,0 +1,12 @@
+const minorValue = 1;
+const majorValue = 100;
+const secretNumber = generateRandomNumber();
+
+function generateRandomNumber() {
+    return parseInt(Math.random() * majorValue + 1);
+}
+
+const minorValueEl = document.getElementById('minor-value');
+const majorValueEl = document.getElementById('major-value');
+minorValueEl.textContent = minorValue;
+majorValueEl.textContent = majorValue;
